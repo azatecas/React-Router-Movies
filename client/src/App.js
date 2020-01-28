7711ba5a83c27a99f132a672de3576c62a82a484
Luis PM <luisp237@gmail.com>
2020-01-28 18:58:24 -0500

    added route, browser router, switch components to App.js, displayed movie cards

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
+import { BrowserRouter as Router,  Route,  Link, Switch } from "react-router-dom";
 
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -12,7 +15,13 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+
+      <Router>
+        <Switch>
+          <Route exact path='/' component={MovieList} />
+          <Route path='/movies/:id' component={Movie} />  
+        </Switch>
+      </Router>
     </div>
   );
 };
